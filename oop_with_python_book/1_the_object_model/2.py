7ffd0d0c4c9c62278682e03626a9ee27a8536e28
Wonnie <wonnie.tech@gmail.com>
2025-12-12 13:28:31 -0800

    Correct exercise #2 for The Object Model chapter
    I actually don't need to accept or store `name` in `__init__` nor do I
    need to print from inside the class.
    
    The two equivalent ways to get the class name from an instance are:
    - `type(obj).__name__`
    - `obj.__class__.__name__`

--- oop_with_python_book/1_the_object_model/2.py
+++ oop_with_python_book/1_the_object_model/2.py
@@ -1,11 +1,7 @@
 class Foo:
-    def __init__(self, name):
-        self.name = name
-        type_name = type(self).__name__
-        print(f"I am a {type_name} object")
-        type_name = self.__class__.__name__
-        print(f"I am a {type_name} object")
-        
-foo = Foo('Foo')
-# I am a Foo object
-# I am a Foo object
+    pass
+
+foo = Foo()
+
+print(f'I am a {type(foo).__name__} object')     # I am a Foo object
+print(f'I am a {foo.__class__.__name__} object') # I am a Foo object
