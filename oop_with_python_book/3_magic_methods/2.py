e0d9b13de214ea1d3818a360ccc5fad43049e082
Wonnie <wonnie.tech@gmail.com>
2025-12-30 13:34:33 -0800

    Add problem 2 of "Magic Methods" chapter

---
+++ oop_with_python_book/3_magic_methods/2.py
@@ -0,0 +1,54 @@
+import math
+
+class Vector:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        if not isinstance(other, Vector):
+            return NotImplemented
+
+        new_x = self.x + other.x
+        new_y = self.y + other.y
+        return Vector(new_x, new_y)
+
+    # __iadd__ method omitted; we don't need it for this exercise
+
+    def __sub__(self, other):
+        if not isinstance(other, Vector):
+            return NotImplemented
+
+        new_x = self.x - other.x
+        new_y = self.y - other.y
+        return Vector(new_x, new_y)
+
+    def __mul__(self, other):
+        if not isinstance(other, Vector):
+            return NotImplemented
+
+        dot_product = ((self.x * other.x) + (self.y * other.y))
+
+        return dot_product
+
+    def __abs__(self):
+        sum_of_squares = ((self.x ** 2) +
+                          (self.y ** 2))
+        return math.sqrt(sum_of_squares)
+
+    def __repr__(self):
+        x = repr(self.x)
+        y = repr(self.y)
+        return f'Vector({x}, {y})'
+
+v1 = Vector(5, 12)
+v2 = Vector(13, -4)
+print(v1 + v2) # Vector(18, 8)
+
+print(v1 - v2) # Vector(-8, 16)
+print(v1 * v2) # 17
+
+print(abs(v1)) # 13.0
+
+
