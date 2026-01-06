d2ad02a4606b1d17b46de70325c05b304dee49ab
Wonnie <wonnie.tech@gmail.com>
2026-01-06 14:12:23 -0800

    Add problem 2 of "Inheritance" chapter in OOP book

---
+++ oop_with_python_book/4_inheritance/2.py
@@ -0,0 +1,36 @@
+class Vehicles:
+    counter = 0
+
+    def __init__(self):
+        Vehicles.counter += 1
+
+    @classmethod
+    def vehicles(cls):
+        return Vehicles.counter
+
+class Car(Vehicles):
+    pass
+
+class Truck(Vehicles):
+    pass
+
+class Boat(Vehicles):
+    pass
+
+print(Car.vehicles())     # 0
+
+car1 = Car()
+print(Car.vehicles())     # 1
+
+car2 = Car()
+car3 = Car()
+car4 = Car()
+print(Car.vehicles())     # 4
+
+truck1 = Truck()
+truck2 = Truck()
+print(Truck.vehicles())   # 6
+
+boat1 = Boat()
+boat2 = Boat()
+print(Boat.vehicles())    # 8
