cf1b6dca12a1ae3efed1fb0891c082b522942b8e
Wonnie <wonnie.tech@gmail.com>
2026-01-09 13:32:11 -0800

    Add initial code from last problem & test code

---
+++ lesson_2/classes_and_objects/3.py
@@ -0,0 +1,43 @@
+class Person:
+
+    def __init__(self, name):
+        name_parts = name.split()
+        self.first_name = name_parts[0]
+        self.last_name = ''
+        if len(name_parts) > 1:
+            self.last_name = name_parts[1]
+
+    @property
+    def name(self):
+        return f'{self.first_name} {self.last_name}'.strip()
+
+    @property
+    def first_name(self):
+        return self._first_name
+
+    @first_name.setter
+    def first_name(self, first_name):
+        self._first_name = first_name
+
+    @property
+    def last_name(self):
+        return self._last_name
+
+    @last_name.setter
+    def last_name(self, last_name):
+        self._last_name = last_name
+
+bob = Person('Robert')
+print(bob.name)             # Robert
+print(bob.first_name)       # Robert
+print(repr(bob.last_name))  # ''
+bob.last_name = 'Smith'
+print(bob.name)             # Robert Smith
+
+bob.name = 'Prince'
+print(bob.first_name)       # Prince
+print(repr(bob.last_name))  # ''
+
+bob.name = 'John Adams'
+print(bob.first_name)       # John
+print(bob.last_name)        # Adams
