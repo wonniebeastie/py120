dc261ef5c33b112e76d71bbc2a72fc2fe85b0d1e
Wonnie <wonnie.tech@gmail.com>
2026-01-09 12:32:17 -0800

    Add last name getter and setter methods

--- lesson_2/classes_and_objects/2.py
+++ lesson_2/classes_and_objects/2.py
@@ -10,10 +10,18 @@ class Person:
     @property
     def first_name(self):
         return self._first_name
-    
+
     @first_name.setter
     def first_name(self, first_name):
         self._first_name = first_name
+
+    @property
+    def last_name(self):
+        return self._last_name
+
+    @last_name.setter
+    def last_name(self, last_name):
+        self._last_name = last_name
 
 bob = Person('Robert')
 print(bob.name)             # Robert
