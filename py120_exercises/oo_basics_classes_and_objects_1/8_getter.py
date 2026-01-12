fc88f24c9cd48e369b949411e98f36a85a5c6679
Wonnie <wonnie.tech@gmail.com>
2026-01-12 14:30:51 -0800

    Make `name` a private attribute & rearrange code
    Previous code did not work because it was calling a non-existent setter.
    Storing the value of `name` to a private instance variable inside the
    initializer rather than defining a setter is fine for this exercise.

--- py120_exercises/oo_basics_classes_and_objects_1/8_getter.py
+++ py120_exercises/oo_basics_classes_and_objects_1/8_getter.py
@@ -1,13 +1,13 @@
 class Cat:
     def __init__(self, name):
-        self.name = name
-
-    def greet(self):
-        print(f"Hello! My name is {self.name}!")
+        self._name = name
 
     @property
     def name(self):
         return self._name
+
+    def greet(self):
+        print(f"Hello! My name is {self.name}!")
 
 kitty = Cat('Sophie')
 kitty.greet() # Hello! My name is Sophie!
