45285851517ca6f1d0fa3d33f1d13da67858352f
Wonnie <wonnie.tech@gmail.com>
2026-01-12 16:19:09 -0800

    Add initial problem code

---
+++ py120_exercises/oo_basics_classes_and_objects_2/2.py
@@ -0,0 +1,18 @@
+class Cat:
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+# Comments show expected output
+kitty = Cat('Sophie')
+print(kitty.name)             # Sophie
+
+kitty.rename('Chloe')
+print(kitty.name)             # Chloe
