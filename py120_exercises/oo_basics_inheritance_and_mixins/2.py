bf6964b1e42d7ae2ae37cfecc7249c9cd3a35d6e
Wonnie <wonnie.tech@gmail.com>
2026-01-13 14:09:07 -0800

    Override superclass initializer with customization

--- py120_exercises/oo_basics_inheritance_and_mixins/2.py
+++ py120_exercises/oo_basics_inheritance_and_mixins/2.py
@@ -7,6 +7,10 @@ class Vehicle:
         return self._year
 
 class Truck(Vehicle):
+    def __init__(self, year):
+        super().__init__(year)
+        self.start_engine()
+
     def start_engine(self):
         print('Ready to go!')
 
