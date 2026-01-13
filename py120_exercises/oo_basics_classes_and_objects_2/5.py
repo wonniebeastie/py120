9969dd99552fb1433358bee9bcca6fa9a52c7ef7
Wonnie <wonnie.tech@gmail.com>
2026-01-12 16:33:57 -0800

    Add initial problem code

---
+++ py120_exercises/oo_basics_classes_and_objects_2/5.py
@@ -0,0 +1,10 @@
+Cat.total()         # 0
+
+kitty1 = Cat()
+Cat.total()         # 1
+
+kitty2 = Cat()
+Cat.total()         # 2
+
+print(Cat())        # <__main__.Cat object at 0x104ed7290>
+Cat.total()         # 3
