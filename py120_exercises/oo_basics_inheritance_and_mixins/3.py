505c55b9a8cbad8e23fdee168eb8374deeb651cb
Wonnie <wonnie.tech@gmail.com>
2026-01-13 14:17:29 -0800

    Add initial problem code

---
+++ py120_exercises/oo_basics_inheritance_and_mixins/3.py
@@ -0,0 +1,23 @@
+class Vehicle:
+    def __init__(self, year):
+        self._year = year
+
+    @property
+    def year(self):
+        return self._year
+
+class Truck(Vehicle):
+    pass
+
+class Car(Vehicle):
+    pass
+
+# Comments show expected output
+truck1 = Truck(1994, 'Short')
+print(truck1.year)            # 1994
+print(truck1.bed_type)        # Short
+
+car1 = Car(2006)
+print(car1.year)              # 2006
+print(car1.bed_type)
+# AttributeError: 'Car' object has no attribute 'bed_type'
