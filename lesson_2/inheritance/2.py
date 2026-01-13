f10bb8e272d4f46faa659e7e1f350d87e51789e7
Wonnie <wonnie.tech@gmail.com>
2026-01-13 12:23:59 -0800

    Add initial problem code

---
+++ lesson_2/inheritance/2.py
@@ -0,0 +1,15 @@
+class Dog:
+    def speak(self):
+        return 'bark!'
+
+    def sleep(self):
+        return 'sleeping!'
+
+    def run(self):
+        return 'running!'
+
+    def jump(self):
+        return 'jumping!'
+
+    def fetch(self):
+        return 'fetching!'
