d9390dd325d43dfeb34aa4ec3880d57f4a868e19
Wonnie <wonnie.tech@gmail.com>
2026-01-12 16:21:19 -0800

    Add initial problem code
    This results in an `AttributeError`.

---
+++ py120_exercises/oo_basics_classes_and_objects_2/3.py
@@ -0,0 +1,17 @@
+class Cat:
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+# Comments show expected output
+kitty = Cat('Sophie')
+print(kitty.identify())
+# <__main__.Cat object at 0x10508c8d0>
+# The object ID (0x105...) may vary
