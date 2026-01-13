b641ddb69fe19b70ccc9fe900485571f0365f8bf
Wonnie <wonnie.tech@gmail.com>
2026-01-12 16:32:52 -0800

    Add method that greets using the `name` getter

--- py120_exercises/oo_basics_classes_and_objects_2/4.py
+++ py120_exercises/oo_basics_classes_and_objects_2/4.py
@@ -6,5 +6,8 @@ class Cat:
     def name(self):
         return self._name
 
+    def personal_greeting(self):
+        print(f"Hello! My name is {self.name}!")
+
 kitty = Cat('Sophie')
-kitty.personal_greeting()     # Hello! My name is Sophie!
+kitty.personal_greeting()  # Hello! My name is Sophie!
