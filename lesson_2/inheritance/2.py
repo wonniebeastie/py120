4f17faba5117206c34d435abcb47d475acce462d
Wonnie <wonnie.tech@gmail.com>
2026-01-13 12:49:13 -0800

    Override superclass behavior & test code
    Try/except so the execution doesn't stop after calling `fetch` on
    `kitty`.

--- lesson_2/inheritance/2.py
+++ lesson_2/inheritance/2.py
@@ -22,3 +22,27 @@ class Bulldog(Dog):
 
     def sleep(self):
         return 'snoring!'
+
+class Cat(Pet):
+
+    def speak(self):
+        return 'meow!'
+
+pet = Pet()
+dave = Dog()
+bud = Bulldog()
+kitty = Cat()
+
+print(pet.run())              # running!
+print(kitty.run())            # running!
+print(kitty.speak())          # meow!
+try:
+    kitty.fetch()
+except Exception as exception:
+    print(exception.__class__.__name__, exception, "\n")
+    # AttributeError 'Cat' object has no attribute 'fetch'
+
+print(dave.speak())           # bark!
+
+print(bud.run())              # running!
+print(bud.sleep())             # "snoring!"
