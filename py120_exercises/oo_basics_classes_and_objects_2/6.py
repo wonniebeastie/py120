2a5fccc8a541f94dd3b95007989a0762c9dd11f3
Wonnie <wonnie.tech@gmail.com>
2026-01-12 17:49:02 -0800

    Add a getter via property decorator

--- py120_exercises/oo_basics_classes_and_objects_2/6.py
+++ py120_exercises/oo_basics_classes_and_objects_2/6.py
@@ -2,7 +2,11 @@ class Cat:
     COLOR = 'purple'
 
     def __init__(self, name):
-        self.name = name
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
 
     def greet(self):
         print(f"Hello! My name is {self.name} and I'm a {Cat.COLOR} cat!")
