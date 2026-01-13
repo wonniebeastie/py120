6ea4d6bf00db970dee310d617047228b08b3dd34
Wonnie <wonnie.tech@gmail.com>
2026-01-13 13:56:06 -0800

    Add initial problem code

---
+++ py120_exercises/oo_basics_inheritance_and_mixins/1.py
@@ -0,0 +1,14 @@
+class Vehicle:
+    def __init__(self, year):
+        self._year = year
+
+    @property
+    def year(self):
+        return self._year
+
+# Comments show expected output
+truck1 = Truck(1994)
+print(truck1.year)            # 1994
+
+car1 = Car(2006)
+print(car1.year)              # 2006
