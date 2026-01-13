602c5493e81f375b51556985c9d50dae58a87f7a
Wonnie <wonnie.tech@gmail.com>
2026-01-12 17:50:15 -0800

    Add initial problem code
    Currently returns `<__main__.Cat object at 0x10543afd0>`

---
+++ py120_exercises/oo_basics_classes_and_objects_2/7.py
@@ -0,0 +1,11 @@
+class Cat:
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+# Comments show expected output
+kitty = Cat('Sophie')
+print(kitty)        # I'm Sophie!
