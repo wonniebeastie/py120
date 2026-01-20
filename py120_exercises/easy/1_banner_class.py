824c77a076f65b72698cc16081b1f379f8c73439
Wonnie <wonnie.tech@gmail.com>
2026-01-20 13:18:12 -0800

    Add initial problem code

---
+++ py120_exercises/easy/1_banner_class.py
@@ -0,0 +1,37 @@
+class Banner:
+    def __init__(self, message):
+        pass
+
+    def __str__(self):
+        return "\n".join([self._horizontal_rule(),
+                          self._empty_line(),
+                          self._message_line(),
+                          self._empty_line(),
+                          self._horizontal_rule()])
+
+    def _empty_line(self):
+        pass
+
+    def _horizontal_rule(self):
+        pass
+
+    def _message_line(self):
+        return f"| {self.message} |"
+
+# Comments show expected output
+
+banner = Banner('To boldly go where no one has gone before.')
+print(banner)
+# +--------------------------------------------+
+# |                                            |
+# | To boldly go where no one has gone before. |
+# |                                            |
+# +--------------------------------------------+
+
+banner = Banner('')
+print(banner)
+# +--+
+# |  |
+# |  |
+# |  |
+# +--+
