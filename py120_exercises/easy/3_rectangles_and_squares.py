389f5a17675ab08336e6f1fd57a8ffdeeefd06d4
Wonnie <wonnie.tech@gmail.com>
2026-01-22 14:49:21 -0800

    Initialize `Squares` with the same lengths

---
+++ py120_exercises/easy/3_rectangles_and_squares.py
@@ -0,0 +1,23 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height
+
+    @property
+    def width(self):
+        return self._width
+
+    @property
+    def height(self):
+        return self._height
+
+    @property
+    def area(self):
+        return self._width * self._height
+
+class Square(Rectangle):
+    def __init__(self, length):
+        super().__init__(length, length)
+
+square = Square(5)
+print(square.area == 25)      # True
