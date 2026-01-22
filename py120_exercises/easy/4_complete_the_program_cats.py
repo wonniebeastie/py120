5c38b2bd6860141438e171af156301b0d2a5ebf0
Wonnie <wonnie.tech@gmail.com>
2026-01-22 15:11:59 -0800

    Add info property to get desired return values

--- py120_exercises/easy/4_complete_the_program_cats.py
+++ py120_exercises/easy/4_complete_the_program_cats.py
@@ -17,8 +17,12 @@ class Cat(Pet):
         self._colors = colors
 
     @property
-    def _colors(self):
+    def colors(self):
         return self._colors
+
+    @property
+    def info(self):
+        return f'My cat {self._name} is {self._age} years old and has {self._colors} fur.'
 
 cocoa = Cat('Cocoa', 3, 'black')
 cheddar = Cat('Cheddar', 4, 'yellow and white')
