d503f81f513ce90d38aa220469e5cdda60c55c8d
Wonnie <wonnie.tech@gmail.com>
2026-01-22 15:07:07 -0800

    Add a way for `Cat` objects to have fur colors

---
+++ py120_exercises/easy/4_complete_the_program_cats.py
@@ -0,0 +1,27 @@
+class Pet:
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def age(self):
+        return self._age
+
+class Cat(Pet):
+    def __init__(self, name, age, colors):
+        super().__init__(name, age)
+        self._colors = colors
+
+    @property
+    def _colors(self):
+        return self._colors
+
+cocoa = Cat('Cocoa', 3, 'black')
+cheddar = Cat('Cheddar', 4, 'yellow and white')
+
+print(cocoa.info) # My cat Cocoa is 3 years old and has black fur.
+print(cheddar.info) # My cat Cheddar is 4 years old and has yellow and white fur.
