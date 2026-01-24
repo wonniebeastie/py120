cb53b8b9205f23abba6e5fd52c5cc620eaaaca2c
Wonnie <wonnie.tech@gmail.com>
2026-01-23 16:44:18 -0800

    Fix `Cat`s to accept only 3 parameters instead

--- py120_exercises/easy/5_animals.py
+++ py120_exercises/easy/5_animals.py
@@ -11,8 +11,13 @@ class Animal:
                 f"{self.age} years old and {self.status}.")
 
 class Cat(Animal):
-    def __init__(self, name, age, legs, species, status):
-        super().__init__(self, name, age, 4, "cat", status)
+    def __init__(self, name, age, status):
+        super().__init__(name, age, 4, "cat", status)
 
     def introduce(self):
-        return super().introduce() + 'Meow meow!'
+        return super().introduce() + ' Meow meow!'
+
+cat = Cat("Pepe", 4, "happy")
+expected = ("Hello, my name is Pepe and I am 4 years old "
+            "and happy. Meow meow!")
+print(cat.introduce() == expected)      # True
