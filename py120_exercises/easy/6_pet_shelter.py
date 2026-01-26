fde26b7f4cde3a8ae7eb9a0a254c9c3efb4c982f
Wonnie <wonnie.tech@gmail.com>
2026-01-26 14:23:45 -0800

    Spread out planning
    Was thinking about this problem wrong - need to read the code and output
    more carefully.

--- py120_exercises/easy/6_pet_shelter.py
+++ py120_exercises/easy/6_pet_shelter.py
@@ -1,30 +1,7 @@
 """
 P: keep track of number of pets adopted per owner
 P: output which pets owner has adopted
-
-Rules:
- - need a method called `print_adoptions` in `Shelter` class
- - in the print statements:
-    - `owner.name` has to be accessed
-    - `number_of_pets` method has to be called on owner instance
-        - the number of pets is tied to each `Owner` instance
-- only 1 `Shelter` instance is needed
-- `shelter.adopt` is called each time for each adopted pet
-    - pet can only be adopted once? (but dunno if this is even needed)
-
-DS/Brainstorm:
- - `print_adoptions` means -> gather pets in a collection & print them
-    - f-string
-    - new line
- - `number_of_pets` can just return the length of that collection
- - each `Owner` instance can have a collection of pets that grows overtime
-    - each time `shelter.adopt` is called for that instance:
-        - add the pet instance to that collection?
- - So.... I want:
-    - a collection (should go in `Owner`)
-    - to print that collection's length (should go in `Shelter`)
 """
-
 class Pet:
     def __init__(self, species, name):
         self.species = species
@@ -32,15 +9,53 @@ class Pet:
 
 class Owner:
     def __init__(self, name):
+        """
+        Each owner should have:
+        - a name
+        - a list of pets they adopted
+        """
         self.name = name
         self.adopted_pets = set()
 
     def number_of_pets(self):
-        return len(self.adopted_pets)
+        """
+        O: the number of pets owner instance has
+        """
+        pass
 
 class Shelter:
+    def __init__(self):
+        """
+        - Each `Shelter` instance should have a masterlist of all owners
+          and which pets they adopted
+        """
     def adopt(self, owner, pet):
-        owner.adopted_pets.add(pet)
+        """
+        I: `Owner` instance
+        O: `Pet` instance
+
+        Rules:
+        - each time someone adopts a pet, their collection of pets should be
+          updated
+        - the "masterlist" for that `Shelter` instance should be updated
+        """
+        pass
+
+    def print_adoptions(self):
+        """
+        Rules:
+        - print "masterlist"
+        - needs access to:
+            - owner
+            - their collection of pets
+
+        DS/Brainstorm:
+        - 
+
+        Algo:
+        - 
+        """
+        pass
 
 cocoa   = Pet('cat', 'Cocoa')
 cheddar = Pet('cat', 'Cheddar')
@@ -62,9 +77,7 @@ shelter.adopt(bholmes, sweetie)
 shelter.adopt(bholmes, molly)
 shelter.adopt(bholmes, chester)
 
-print(phanson.adopted_pets)
-
-# shelter.print_adoptions()
+shelter.print_adoptions()
 # print(f"{phanson.name} has {phanson.number_of_pets()} "
 #       "adopted pets.")
 # print(f"{bholmes.name} has {bholmes.number_of_pets()} "
