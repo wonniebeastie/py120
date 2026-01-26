cbd38dc339397ea12241937705e80168317045fe
Wonnie <wonnie.tech@gmail.com>
2026-01-26 11:59:12 -0800

    Add initial problem code

---
+++ py120_exercises/easy/6_pet_shelter.py
@@ -0,0 +1,40 @@
+cocoa   = Pet('cat', 'Cocoa')
+cheddar = Pet('cat', 'Cheddar')
+darwin  = Pet('bearded dragon', 'Darwin')
+kennedy = Pet('dog', 'Kennedy')
+sweetie = Pet('parakeet', 'Sweetie Pie')
+molly   = Pet('dog', 'Molly')
+chester = Pet('fish', 'Chester')
+
+phanson = Owner('P Hanson')
+bholmes = Owner('B Holmes')
+
+shelter = Shelter()
+shelter.adopt(phanson, cocoa)
+shelter.adopt(phanson, cheddar)
+shelter.adopt(phanson, darwin)
+shelter.adopt(bholmes, kennedy)
+shelter.adopt(bholmes, sweetie)
+shelter.adopt(bholmes, molly)
+shelter.adopt(bholmes, chester)
+
+shelter.print_adoptions()
+print(f"{phanson.name} has {phanson.number_of_pets()} "
+      "adopted pets.")
+print(f"{bholmes.name} has {bholmes.number_of_pets()} "
+      "adopted pets.")
+
+# The outputs we want:
+# P Hanson has adopted the following pets:
+# a cat named Cocoa
+# a cat named Cheddar
+# a bearded dragon named Darwin
+
+# B Holmes has adopted the following pets:
+# a dog named Molly
+# a parakeet named Sweetie Pie
+# a dog named Kennedy
+# a fish named Chester
+
+# P Hanson has 3 adopted pets.
+# B Holmes has 4 adopted pets.
