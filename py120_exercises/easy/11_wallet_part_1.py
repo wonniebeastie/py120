c75c14c1932f406f2ecb6208265fdb0e0895541d
Wonnie <wonnie.tech@gmail.com>
2026-01-27 14:37:34 -0800

    Customize the way + behaves to add custom objects

--- py120_exercises/easy/11_wallet_part_1.py
+++ py120_exercises/easy/11_wallet_part_1.py
@@ -2,6 +2,12 @@ class Wallet:
     def __init__(self, amount):
         self.amount = amount
 
+    def __add__(self, other):
+        if isinstance(other, Wallet):
+            return Wallet(self.amount + other.amount)
+        
+        return NotImplemented
+
 wallet1 = Wallet(50)
 wallet2 = Wallet(30)
 merged_wallet = wallet1 + wallet2
