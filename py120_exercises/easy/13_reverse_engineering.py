f194927115736ff455c4895b5fc5d329f8449819
Wonnie <wonnie.tech@gmail.com>
2026-01-27 15:11:17 -0800

    Add method to get first output correct

--- py120_exercises/easy/13_reverse_engineering.py
+++ py120_exercises/easy/13_reverse_engineering.py
@@ -1,3 +1,10 @@
+class Transform:
+    def __init__(self, data):
+        self.data = data
+
+    def uppercase(self):
+        return self.data.upper()
+
 my_data = Transform('abc')
 
 # Should output:
