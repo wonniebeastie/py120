fc50e7490690d85a9ca3a25314032f289c8ced45
Wonnie <wonnie.tech@gmail.com>
2026-01-27 14:43:17 -0800

    Add initial problem code for easy 12

---
+++ py120_exercises/easy/12_wallet_part_2.py
@@ -0,0 +1,11 @@
+class Wallet:
+    def __init__(self, amount):
+        self.amount = amount
+
+    def __add__(self, other):
+        return Wallet(self.amount + other.amount)
+
+wallet1 = Wallet(50)
+wallet2 = Wallet(30)
+merged_wallet = wallet1 + wallet2
+print(merged_wallet)          # Wallet with $80.
