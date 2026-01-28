583af3aae7681900a752f23d733ade7144e05c0e
Wonnie <wonnie.tech@gmail.com>
2026-01-28 14:57:15 -0800

    Add initial test code for medium problem 1

---
+++ py120_exercises/medium/1_circular_buffer.py
@@ -0,0 +1,40 @@
+buffer = CircularBuffer(3)
+
+print(buffer.get() is None)          # True
+
+buffer.put(1)
+buffer.put(2)
+print(buffer.get() == 1)             # True
+
+buffer.put(3)
+buffer.put(4)
+print(buffer.get() == 2)             # True
+
+buffer.put(5)
+buffer.put(6)
+buffer.put(7)
+print(buffer.get() == 5)             # True
+print(buffer.get() == 6)             # True
+print(buffer.get() == 7)             # True
+print(buffer.get() is None)          # True
+
+buffer2 = CircularBuffer(4)
+
+print(buffer2.get() is None)         # True
+
+buffer2.put(1)
+buffer2.put(2)
+print(buffer2.get() == 1)            # True
+
+buffer2.put(3)
+buffer2.put(4)
+print(buffer2.get() == 2)            # True
+
+buffer2.put(5)
+buffer2.put(6)
+buffer2.put(7)
+print(buffer2.get() == 4)            # True
+print(buffer2.get() == 5)            # True
+print(buffer2.get() == 6)            # True
+print(buffer2.get() == 7)            # True
+print(buffer2.get() is None)         # True
