2f008db806739f63fc7aa92bae98046bd7452de9
Wonnie <wonnie.tech@gmail.com>
2026-01-29 14:45:17 -0800

    Add more to PEDAC in comments
    Just doing a walkthrough of the problem, at the planning stage.

--- py120_exercises/medium/1_circular_buffer.py
+++ py120_exercises/medium/1_circular_buffer.py
@@ -2,11 +2,83 @@ class CircularBuffer:
     """
     Initializes a buffer for each instance
 
-    P: Maintain a fixed-size list that "wraps around".
+    P:
+    Maintain a fixed-size list that "wraps around".
+
+    EX:
+    - `buffer = CircularBuffer(3)`
+        - `size` = 3
+        - `buffer` = [None, None, None]
+        - `next` = 0
+        - `oldest` = 0
+        ==> `put(1)`
+            - buffer[0] = 1
+            - buffer = [1, None, None]
+        ==> `put(2)`
+            - buffer[]
+        - VISUAL:
+            [0, 1, 2]
+
+            [None, None, None] empty
+            [1, None, None] -> None is replaced by newest (1)
+            [1, 2, None] -> None is replaced by newest (2)
+            [None, 2, None] -> oldest (1) removed, replaced by None
+            [None, 2, 3] -> None is replaced by newest (3)
+            [4, 2, 3] -> None is replaced by newest (4) - FULL
+            [4, None, 3] -> oldest (2) removed, replaced by None
+            [4, 5, 3] -> None is replaced by newest (5) - FULL
+            [4, 5, 6] -> oldest (3) is replaced by newest (6) - FULL
+            [7, 5, 6] -> oldest (4) is replaced by newest (7) - FULL
+            [7, None, 6] -> oldest (5) is removed, replaced by None
+            [7, None, None] -> oldest (6) is removed, replaced by None
+            [None, None, None] -> oldest (7) is removed, replaced by None
+            [] -> 
+
+    OVERVIEW:
+    - `put` always writes at the "next" position
+    - `get` always removes/returns the "oldest" item
+    - if buffer is full, `put` overwrites the oldest
+    - if buffer is empty, `get` returns `None`
     
-    Need to know:
-    - where to insert (index where the NEXT inserted item will go)
-    - where to remove from (index of the OLDEST item)
+    BRAINSTORM:
+    - Need to know:
+        - where to insert (index where the NEXT inserted item will go)
+        - where to remove from (index of the OLDEST item)
+    -----
+    - use a list of fixed length `size`
+    - use 2 integers:
+        - `next`: index to write to next
+        - `oldest`: index of item to remove next
+    - use `None` to mark "empty" slots
+
+    METHODS:
+    - `put`: Adds an object to the buffer
+        ----
+        I: int, new value to be added (`new`)
+        O: N/A, mutates `buffer` list
+    ====
+    - `get`:
+        Removes & returns the oldest object in the buffer.
+        Returns `None` if buffer is empty
+        ----
+        I: N/A
+        O: `None` or `oldest_item`
+
+    ALGO:
+    - `put(new)`
+        - if `buffer` slot at `next` is empty:
+            - assign `new` to slot
+            - advance `next` (wrap with `% size`)
+
+    - `get()`
+        - check item at index `oldest`:
+            - if item is `None` (means buffer is empty):
+                - return `None`
+            - else (means slot is filled):
+                - store that item in `oldest_item`
+                - set `buffer[oldest]` to `None`
+                - advance `oldest` (wrap with `% size`)
+                - return `oldest_item`
     """
     def __init__(self, size):
         self.size = size
@@ -15,43 +87,10 @@ class CircularBuffer:
         self.oldest = 0
 
     def put(self, new):
-        """
-        Adds an object to the buffer
-
-        P: 
-        - `put` always writes at the "next" position
-        - If buffer is full, `put` overwrites the oldest
-        """
-        self.buffer[self.next] = new
+        pass
 
     def get(self):
-        """
-        Removes & returns the oldest object in the buffer.
-        Returns `None` if buffer is empty
-
-        P: 
-        -`get` always removes/returns the "oldest" item
-        - If buffer is empty, `get` returns `None`
-        """
         pass
-"""
-[0, 1, 2]
-
-[None, None, None] empty
-[1, None, None] -> None is replaced by newest (1)
-[1, 2, None] -> None is replaced by newest (2)
-[None, 2, None] -> oldest (1) removed, replaced by None
-[None, 2, 3] -> None is replaced by newest (3)
-[4, 2, 3] -> None is replaced by newest (4) - FULL
-[4, None, 3] -> oldest (2) removed, replaced by None
-[4, 5, 3] -> None is replaced by newest (5) - FULL
-[4, 5, 6] -> oldest (3) is replaced by newest (6) - FULL
-[7, 5, 6] -> oldest (4) is replaced by newest (7) - FULL
-[7, None, 6] -> oldest (5) is removed, replaced by None
-[7, None, None] -> oldest (6) is removed, replaced by None
-[None, None, None] -> oldest (7) is removed, replaced by None
-[] -> 
-"""
 
 buffer = CircularBuffer(3)
 
