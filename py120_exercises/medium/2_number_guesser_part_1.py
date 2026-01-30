062ce98cc9011e2b209a98b9197b3cd474fb47cf
Wonnie <wonnie.tech@gmail.com>
2026-01-30 13:32:05 -0800

    Add initial problem code for medium problem 2

---
+++ py120_exercises/medium/2_number_guesser_part_1.py
@@ -0,0 +1,58 @@
+game = GuessingGame()
+game.play()
+
+# You have 7 guesses remaining.
+# Enter a number between 1 and 100: 104
+# Invalid guess. Enter a number between 1 and 100: 50
+# Your guess is too low.
+
+# You have 6 guesses remaining.
+# Enter a number between 1 and 100: 75
+# Your guess is too low.
+
+# You have 5 guesses remaining.
+# Enter a number between 1 and 100: 85
+# Your guess is too high.
+
+# You have 4 guesses remaining.
+# Enter a number between 1 and 100: 0
+# Invalid guess. Enter a number between 1 and 100: 80
+# Your guess is too low.
+
+# You have 3 guesses remaining.
+# Enter a number between 1 and 100: 81
+# That's the number!
+
+# You won!
+
+game.play()
+
+# You have 7 guesses remaining.
+# Enter a number between 1 and 100: 50
+# Your guess is too high.
+
+# You have 6 guesses remaining.
+# Enter a number between 1 and 100: 25
+# Your guess is too low.
+
+# You have 5 guesses remaining.
+# Enter a number between 1 and 100: 37
+# Your guess is too high.
+
+# You have 4 guesses remaining.
+# Enter a number between 1 and 100: 31
+# Your guess is too low.
+
+# You have 3 guesses remaining.
+# Enter a number between 1 and 100: 34
+# Your guess is too high.
+
+# You have 2 guesses remaining.
+# Enter a number between 1 and 100: 32
+# Your guess is too low.
+
+# You have 1 guess remaining.
+# Enter a number between 1 and 100: 32
+# Your guess is too low.
+
+# You have no more guesses. You lost!
