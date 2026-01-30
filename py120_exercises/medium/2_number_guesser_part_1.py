3e58e1380dd3bfeba4719846307b19254d63abd0
Wonnie <wonnie.tech@gmail.com>
2026-01-30 13:33:39 -0800

    Identify rules and flow of game in comment

--- py120_exercises/medium/2_number_guesser_part_1.py
+++ py120_exercises/medium/2_number_guesser_part_1.py
@@ -1,3 +1,31 @@
+"""
+Rules:
+- a random number is generated at the beginning of the game
+- 7 guesses per game
+- each "turn" looped until either:
+    - player runs out of guesses
+    - guesses correctly and wins
+- if player guesses wrongly, display whether the guess was too low or too high
+- a game object should start a NEW game with a NEW number to guess with each
+  call to `play`
+- class should be called `GuessingGame`
+
+Flow:
+1) random number is generated (hidden)
+2) display number of guesses remaining
+3) ask player to input a number b/w 1 ~ 100 (inclusive)
+    => guess is invalid:
+        - display that the guess is invalid, and ask to enter again
+    => guess is valid:
+        - player guesses wrong:
+            - display whether the guess is too high or too low
+            - decrement number of guesses by 1
+            - loop back to #2
+        - player guesses correctly:
+            - display "That's the number!"
+            - display that they won
+            - end game
+"""
 game = GuessingGame()
 game.play()
 
