774c593d936f468d91bd88365d32d5ea3f8d7d84
Wonnie <wonnie.tech@gmail.com>
2026-02-03 14:49:41 -0800

    Customize `print()` to output proper card string

--- py120_exercises/medium/4_highest_and_lowest_ranking_cards.py
+++ py120_exercises/medium/4_highest_and_lowest_ranking_cards.py
@@ -14,7 +14,7 @@ Rules:
 
 Methods:
 - any needed to determine lowest & highest
-- custom `__str__` method:
+[] custom `__str__` method:
     - returns a str representation of the card like "Jack of Diamonds"
       or "4 of Clubs", etc.
 """
@@ -25,36 +25,39 @@ class Card:
         self.rank = rank
         self.suit = suit
 
+    def __str__(self):
+        return f"{self.rank} of {self.suit}"
+
 # Test code
 cards = [Card(2, 'Hearts'),
          Card(10, 'Diamonds'),
          Card('Ace', 'Clubs')]
-print(min(cards) == Card(2, 'Hearts'))             # True
-print(max(cards) == Card('Ace', 'Clubs'))          # True
-print(str(min(cards)) == "2 of Hearts")            # True
-print(str(max(cards)) == "Ace of Clubs")           # True
+# print(min(cards) == Card(2, 'Hearts'))             # True
+# print(max(cards) == Card('Ace', 'Clubs'))          # True
+# print(str(min(cards)) == "2 of Hearts")            # True
+# print(str(max(cards)) == "Ace of Clubs")           # True
 
 cards = [Card(5, 'Hearts')]
-print(min(cards) == Card(5, 'Hearts'))             # True
-print(max(cards) == Card(5, 'Hearts'))             # True
+# print(min(cards) == Card(5, 'Hearts'))             # True
+# print(max(cards) == Card(5, 'Hearts'))             # True
 print(str(Card(5, 'Hearts')) == "5 of Hearts")     # True
 
 cards = [Card(4, 'Hearts'),
          Card(4, 'Diamonds'),
          Card(10, 'Clubs')]
-print(min(cards).rank == 4)                        # True
-print(max(cards) == Card(10, 'Clubs'))             # True
+# print(min(cards).rank == 4)                        # True
+# print(max(cards) == Card(10, 'Clubs'))             # True
 print(str(Card(10, 'Clubs')) == "10 of Clubs")     # True
 
 cards = [Card(7, 'Diamonds'),
          Card('Jack', 'Diamonds'),
          Card('Jack', 'Spades')]
-print(min(cards) == Card(7, 'Diamonds'))           # True
-print(max(cards).rank == 'Jack')                   # True
+# print(min(cards) == Card(7, 'Diamonds'))           # True
+# print(max(cards).rank == 'Jack')                   # True
 print(str(Card(7, 'Diamonds')) == "7 of Diamonds") # True
 
 cards = [Card(8, 'Diamonds'),
          Card(8, 'Clubs'),
          Card(8, 'Spades')]
-print(min(cards).rank == 8)                        # True
-print(max(cards).rank == 8)                        # True
+# print(min(cards).rank == 8)                        # True
+# print(max(cards).rank == 8)                        # True
