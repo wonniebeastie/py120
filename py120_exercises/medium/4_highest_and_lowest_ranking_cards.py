a298a7d78919affdb21cbdc757f468f650734100
Wonnie <wonnie.tech@gmail.com>
2026-02-03 16:14:34 -0800

    Add comparison dunders for rest of test cases

--- py120_exercises/medium/4_highest_and_lowest_ranking_cards.py
+++ py120_exercises/medium/4_highest_and_lowest_ranking_cards.py
@@ -46,18 +46,24 @@ class Card:
     def value(self):
         return Card.RANK_NUMBERS.get(self.rank, self.rank)
 
+    def __lt__(self, other):
+        return self.value < other.value
+
+    def __eq__(self, other):
+        return self.rank == other.rank and self.suit == other.suit
+
 # Test code
 cards = [Card(2, 'Hearts'),
          Card(10, 'Diamonds'),
          Card('Ace', 'Clubs')]
 print(min(cards) == Card(2, 'Hearts'))             # True
-# print(max(cards) == Card('Ace', 'Clubs'))          # True
-# print(str(min(cards)) == "2 of Hearts")            # True
-# print(str(max(cards)) == "Ace of Clubs")           # True
+print(max(cards) == Card('Ace', 'Clubs'))          # True
+print(str(min(cards)) == "2 of Hearts")            # True
+print(str(max(cards)) == "Ace of Clubs")           # True
 
 cards = [Card(5, 'Hearts')]
-# print(min(cards) == Card(5, 'Hearts'))             # True
-# print(max(cards) == Card(5, 'Hearts'))             # True
+print(min(cards) == Card(5, 'Hearts'))             # True
+print(max(cards) == Card(5, 'Hearts'))             # True
 print(str(Card(5, 'Hearts')) == "5 of Hearts")     # True
 
 cards = [Card(4, 'Hearts'),
@@ -70,12 +76,12 @@ print(str(Card(10, 'Clubs')) == "10 of Clubs")     # True
 cards = [Card(7, 'Diamonds'),
          Card('Jack', 'Diamonds'),
          Card('Jack', 'Spades')]
-# print(min(cards) == Card(7, 'Diamonds'))           # True
-# print(max(cards).rank == 'Jack')                   # True
+print(min(cards) == Card(7, 'Diamonds'))           # True
+print(max(cards).rank == 'Jack')                   # True
 print(str(Card(7, 'Diamonds')) == "7 of Diamonds") # True
 
 cards = [Card(8, 'Diamonds'),
          Card(8, 'Clubs'),
          Card(8, 'Spades')]
-# print(min(cards).rank == 8)                        # True
-# print(max(cards).rank == 8)                        # True
+print(min(cards).rank == 8)                        # True
+print(max(cards).rank == 8)                        # True
