e86e6e71dd14c0cfbf0dca84dc5d270ee3c18c16
Wonnie <wonnie.tech@gmail.com>
2026-02-04 12:12:09 -0800

    Add problem starting code & rules

---
+++ py120_exercises/medium/5_deck_of_cards.py
@@ -0,0 +1,41 @@
+class Deck:
+    RANKS = list(range(2, 11)) + ['Jack', 'Queen', 'King', 'Ace']
+    SUITS = ['Hearts', 'Clubs', 'Diamonds', 'Spades']
+
+class Card:
+    RANK_NUMBERS = {"Jack": 11, "Queen": 12, "King": 13, "Ace": 14}
+
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    def __str__(self):
+        return f"{self.rank} of {self.suit}"
+
+    @property
+    def value(self):
+        return Card.RANK_NUMBERS.get(self.rank, self.rank)
+
+    def __lt__(self, other):
+        return self.value < other.value
+
+    def __eq__(self, other):
+        return self.rank == other.rank and self.suit == other.suit
+
+# Test code
+deck = Deck()
+drawn = []
+for _ in range(52):
+    drawn.append(deck.draw())
+
+count_rank_5 = sum([1 for card in drawn if card.rank == 5])
+count_hearts = sum([1 for card in drawn if card.suit == 'Hearts'])
+
+print(count_rank_5 == 4)      # True
+print(count_hearts == 13)     # True
+
+drawn2 = []
+for _ in range(52):
+    drawn2.append(deck.draw())
+
+print(drawn != drawn2)        # True (Almost always).
