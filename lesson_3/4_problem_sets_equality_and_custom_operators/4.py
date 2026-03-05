8b1bb902474a72ed56b9799fc8fbd6117bdabc5b
Wonnie <wonnie.tech@gmail.com>
2026-03-05 12:17:51 -0800

    Add `__sub__` method for `-` operator

--- lesson_3/4_problem_sets_equality_and_custom_operators/4.py
+++ lesson_3/4_problem_sets_equality_and_custom_operators/4.py
@@ -23,6 +23,14 @@ class Vector:
         y = self.y + other.y
         return Vector(x, y)
 
+    def __sub__(self, other):
+        if not isinstance(other, Vector):
+            return NotImplemented
+        
+        x = self.x - other.x
+        y = self.y - other.y
+        return Vector(x, y)
+
 print(Vector(3, 2) + Vector(5, 12))   # Vector(8, 14)
 print(Vector(5, 12) - Vector(3, 2))   # Vector(2, 10)
 print(Vector(5, 12) * 2)              # Vector(10, 24)
