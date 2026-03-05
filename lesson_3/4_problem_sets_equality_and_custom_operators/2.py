040b1de9c1e2fad37086918162b2f631ca028704
Wonnie <wonnie.tech@gmail.com>
2026-03-04 16:38:03 -0800

    Add solution to #2 of lesson 3 assignment 4

---
+++ lesson_3/4_problem_sets_equality_and_custom_operators/2.py
@@ -0,0 +1,45 @@
+"""
+[x] Compare `Cat` objects for equality & inequality by their `name` value
+[] Ignore case
+[x] Should work for `==` & `!=` operators
+[x] If right-hand operand is not a `Cat` object, methods should return
+`NotImplemented`.
+[x] Write test cases to demonstrate the methods work as intended.
+"""
+class Cat:
+    def __init__(self, name):
+        self.name = name
+
+    def __eq__(self, other):
+        if not isinstance(other, Cat):
+            return NotImplemented
+
+        return self.name.casefold() == other.name.casefold()
+
+    def __ne__(self, other):
+        if not isinstance(other, Cat):
+            return NotImplemented
+
+        return self.name.casefold() != other.name.casefold()
+
+floof = Cat('Floof')
+floof2 = Cat('Floof')
+frog_paste = Cat('Frog Paste')
+
+print(floof == frog_paste) # False
+print(floof == floof2)     # True
+
+print(floof != frog_paste) # True
+print(floof != floof2)     # False
+
+
+# These test cases are here to show if the right-hand operand is not a `Cat`,
+# the methods return `NotImplemented`
+print(floof == 'Floof')  # False
+print(floof != 'Floof')  # True
+
+floof_lower = Cat('floof')
+
+# Case-insensitive
+print(floof == floof_lower) # True
+print(floof != floof_lower) # False
