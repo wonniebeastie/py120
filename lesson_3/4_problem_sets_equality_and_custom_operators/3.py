56fda6503be27b66056c5bf8da50e58896bc7585
Wonnie <wonnie.tech@gmail.com>
2026-03-04 17:39:01 -0800

    Add rest of solution to #3 Lesson 3, Assignment 4

--- lesson_3/4_problem_sets_equality_and_custom_operators/3.py
+++ lesson_3/4_problem_sets_equality_and_custom_operators/3.py
@@ -1,9 +1,9 @@
 """
-[] Create methods for ordered comparisons of `Cat` objects by `name` value.
-[] Comparison should ignore case.
-[] Methods should work for `<`, `<=`, `>`, and `>=` operators.
-[] If right-hand operand is not a `Cat` object, return `NotImplemented`.
-[] Write test cases to demonstrate viability of methods.
+[x] Create methods for ordered comparisons of `Cat` objects by `name` value.
+[x] Comparison should ignore case.
+[x] Methods should work for `<`, `<=`, `>`, and `>=` operators.
+[x] If right-hand operand is not a `Cat` object, return `NotImplemented`.
+[x] Write test cases to demonstrate viability of methods.
 """
 class Cat:
     def __init__(self, name):
@@ -21,24 +21,46 @@ class Cat:
 
         return self.name.casefold() != other.name.casefold()
 
+    def __lt__(self, other):
+        if not isinstance(other, Cat):
+            return NotImplemented
+
+        return self.name.casefold() < other.name.casefold()
+
+    def __gt__(self, other):
+        if not isinstance(other, Cat):
+            return NotImplemented
+
+        return self.name.casefold() > other.name.casefold()
+
+    def __le__(self, other):
+        if not isinstance(other, Cat):
+            return NotImplemented
+
+        return self.name.casefold() <= other.name.casefold()
+
+    def __ge__(self, other):
+        if not isinstance(other, Cat):
+            return NotImplemented
+
+        return self.name.casefold() >= other.name.casefold()
+
 floof = Cat('Floof')
 floof2 = Cat('Floof')
 frog_paste = Cat('Frog Paste')
 
-print(floof == frog_paste) # False
-print(floof == floof2)     # True
+print(floof < frog_paste)  # True
+print(floof > frog_paste)  # False
+print(floof < floof2)      # False
 
-print(floof != frog_paste) # True
-print(floof != floof2)     # False
+print(floof <= frog_paste) # True
+print(frog_paste <= floof) # False
+print(floof <= floof2)     # True
 
+print(floof > frog_paste) # False
+print(frog_paste > floof) # True
+print(floof > floof2)     # False
 
-# These test cases are here to show if the right-hand operand is not a `Cat`,
-# the methods return `NotImplemented`
-print(floof == 'Floof')  # False
-print(floof != 'Floof')  # True
-
-floof_lower = Cat('floof')
-
-# Case-insensitive
-print(floof == floof_lower) # True
-print(floof != floof_lower) # False
+print(floof >= frog_paste) # False
+print(frog_paste >= floof) # True
+print(floof >= floof2)     # True
