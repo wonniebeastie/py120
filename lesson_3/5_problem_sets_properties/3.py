a95e83e2e79b622452a851a7590b9a8d23edaf85
Wonnie <wonnie.tech@gmail.com>
2026-03-10 14:54:23 -0700

    Add properties for width and height

--- lesson_3/5_problem_sets_properties/3.py
+++ lesson_3/5_problem_sets_properties/3.py
@@ -2,3 +2,12 @@ class Rectangle:
     def __init__(self, width, height):
         self._width = width
         self._height = height
+
+    @property
+    def width(self):
+        return self._width
+
+    @property
+    def height(self):
+        return self._height
+
