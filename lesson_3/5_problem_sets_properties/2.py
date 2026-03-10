e328ff169cc7fde521b94056c6b76f74a50a8857
Wonnie <wonnie.tech@gmail.com>
2026-03-10 14:33:18 -0700

    Add initial code from previous problem

---
+++ lesson_3/5_problem_sets_properties/2.py
@@ -0,0 +1,22 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if not isinstance(name, str):
+            print("Name must be a string.")
+        
+        self._name = name
+
+luna = Person("Luna")
+print(luna.name) # Luna
+
+luna.name = "Luna Lovegood"
+print(luna.name) # Luna Lovegood
+
+not_person = Person(56) # Name must be a string.
