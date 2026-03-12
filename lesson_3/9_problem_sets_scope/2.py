f5dc6fe4706eedddc2d9839cd76cb3f8d6f337e8
Wonnie <wonnie.tech@gmail.com>
2026-03-12 15:29:26 -0700

    Add initial set up for problem

---
+++ lesson_3/9_problem_sets_scope/2.py
@@ -0,0 +1,15 @@
+"""
+Add a `get_breed` method to the `Dog` class from your answer to the previous 
+problem. The method should return the dog's breed. Use the method to print the 
+breeds of the two dog objects you created in the previous problem. You should 
+also mark the `breed` instance variable for internal use only.
+"""
+class Dog:
+    def __init__(self, breed):
+        self.breed = breed
+
+doggo1 = Dog('Golden Retriever')
+doggo2 = Dog('Poodle')
+
+print(doggo1.breed) # Golden Retriever
+print(doggo2.breed) # Poodle
