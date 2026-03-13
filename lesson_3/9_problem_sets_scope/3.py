9d86541321c354fcb2587c78d7bc3956694c4782
Wonnie <wonnie.tech@gmail.com>
2026-03-13 15:25:49 -0700

    Create `Cat` class

---
+++ lesson_3/9_problem_sets_scope/3.py
@@ -0,0 +1,8 @@
+"""
+Create a `Cat` class that has a single method named `get_name` that returns the
+`name` instance variable. Without initializing `name`, try to instantiate a 
+`Cat` object and call `get_name`. Print `Name not set!` when the error occurs.
+"""
+class Cat:
+    def __init__(self, name):
+        self.name = name
