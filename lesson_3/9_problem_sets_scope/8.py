5f2ada402f52f6548fb6ed4decf1f4e69bce40bb
Wonnie <wonnie.tech@gmail.com>
2026-03-14 13:06:42 -0700

    Add instance method to print both var names
    Added test code

--- lesson_3/9_problem_sets_scope/8.py
+++ lesson_3/9_problem_sets_scope/8.py
@@ -9,3 +9,12 @@ class Car:
 
     def __init__(self, manufacturer):
         self.manufacturer = manufacturer
+
+    def show_manufacturer(self):
+        print(self.__class__.manufacturer)
+        print(self.manufacturer)
+
+car = Car("Hogwarts")
+car.show_manufacturer()
+# McDs
+# Hogwarts
