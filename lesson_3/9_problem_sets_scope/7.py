b00f3d237589e198baf7f18e7883fe66e4c7f4ff
Wonnie <wonnie.tech@gmail.com>
2026-03-14 12:40:54 -0700

    Add problem description & code from prev prob

---
+++ lesson_3/9_problem_sets_scope/7.py
@@ -0,0 +1,12 @@
+"""
+Modify the `Student` class from your answer to the previous problem. The 
+modified class should have a class method that returns the school's name. 
+Without instantiating any `Student` objects, print the school's name in two 
+different ways: once with the class method, and once by accessing the class 
+variable directly.
+"""
+class Student:
+    school_name = 'Oxford'
+
+    def __init__(self, name):
+        self.name = name
