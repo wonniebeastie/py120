ead315275c1de1e411fd85c8052f005033bba736
Wonnie <wonnie.tech@gmail.com>
2026-03-16 15:56:28 -0700

    Add initial problem question & code

---
+++ lesson_3/9_problem_sets_scope/14.py
@@ -0,0 +1,13 @@
+# Consider the following code:
+class A:
+  def __init__(self):
+      self.var_a = "A class variable"
+
+class B(A):
+    def __init__(self):
+        self.var_b = "B class variable"
+
+b = B()
+print(b.var_a)
+
+# Without running this code, what will happen if you were to run it? Why?
