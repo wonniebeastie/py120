5ce335ed4e1e5cde1084adf70976383e628ef584
Wonnie <wonnie.tech@gmail.com>
2026-03-16 14:57:34 -0700

    Create `Sparrow` class and print its species

--- lesson_3/9_problem_sets_scope/9.py
+++ lesson_3/9_problem_sets_scope/9.py
@@ -6,3 +6,9 @@ instance object, then print its species. The expected output is `sparrow`.
 class Bird:
     def __init__(self, species):
         self.species = species
+
+class Sparrow(Bird):
+    pass
+
+sparrow1 = Sparrow("Sparrow")
+print(sparrow1.species) # Sparrow
