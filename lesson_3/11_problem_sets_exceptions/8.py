0130d063100093a0454200a451160dc8db2bcf35
Wonnie <wonnie.tech@gmail.com>
2026-03-25 15:09:54 -0700

    Try to return the age of a student if name exists

--- lesson_3/11_problem_sets_exceptions/8.py
+++ lesson_3/11_problem_sets_exceptions/8.py
@@ -1,1 +1,10 @@
 students = {'John': 25, 'Jane': 22, 'Doe': 30}
+
+def get_age(name):
+    try:
+        return students[name]
+    except KeyError:
+        return 'Student not found'
+
+print(get_age('John')) # 25
+print(get_age('Dobby')) # Student not found
