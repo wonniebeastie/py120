67208341082d99cd036476b082c7a8a5b3e89ed9
Wonnie <wonnie.tech@gmail.com>
2026-03-30 15:41:44 -0700

    Have classes inherit from `SpeedMixin`

--- lesson_4/3.py
+++ lesson_4/3.py
@@ -2,10 +2,10 @@ class SpeedMixin:
     def go_fast(self):
         print(f'I am a super fast {self.__class__.__name__}')
 
-class Car:
+class Car(SpeedMixin):
     def go_slow(self):
         print('I am safe and driving slow.')
 
-class Truck:
+class Truck(SpeedMixin):
     def go_very_slow(self):
         print('I am a heavy truck and like going very slow.')
