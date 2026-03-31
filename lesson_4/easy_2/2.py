ff2fb1267b0c3a9cf931ccd6782dc45cc9802b6c
Wonnie <wonnie.tech@gmail.com>
2026-03-31 11:31:52 -0700

    Add class variable to count number of games

--- lesson_4/easy_2/2.py
+++ lesson_4/easy_2/2.py
@@ -1,4 +1,6 @@
 class Game:
+    count = 0
+
     def play(self):
         return 'Start the game!'
 
@@ -10,10 +12,10 @@ print(Game.count)                       # 1
 print(bingo.play())                     # Start the Bingo game!
 print(bingo.player_name)                # Bill
 
-scrabble = Scrabble('Scrabble', 'Jill', 'Sill')
-print(Game.count)                       # 2
-print(scrabble.play())                  # Start the Scrabble game!
-print(scrabble.player_name1)            # Jill
-print(scrabble.player_name2)            # Sill
-print(scrabble.player_name)
-# AttributeError: 'Scrabble' object has no attribute 'player_name'
+# scrabble = Scrabble('Scrabble', 'Jill', 'Sill')
+# print(Game.count)                       # 2
+# print(scrabble.play())                  # Start the Scrabble game!
+# print(scrabble.player_name1)            # Jill
+# print(scrabble.player_name2)            # Sill
+# print(scrabble.player_name)
+# # AttributeError: 'Scrabble' object has no attribute 'player_name'
