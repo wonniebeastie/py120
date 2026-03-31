0ee81f5dabed776aea01004f43d588fb3b5a009e
Wonnie <wonnie.tech@gmail.com>
2026-03-31 12:08:13 -0700

    Remove reference to `player_name` in `Game`
    Was first testing with just the first part of the test code, but after
    adding the `Scrabble` class, realized that `player_name` should not be
    a parameter/instance variable of the superclass.

--- lesson_4/easy_2/2.py
+++ lesson_4/easy_2/2.py
@@ -1,9 +1,8 @@
 class Game:
     count = 0
 
-    def __init__(self, game_type, player_name):
+    def __init__(self, game_type):
         self.game_type = game_type
-        self.player_name = player_name
         Game.count += 1
 
     def play(self):
@@ -14,17 +13,25 @@ class Game:
         return cls.count
 
 class Bingo(Game):
-    pass
+    def __init__(self, game_type, player_name):
+        super().__init__(game_type)
+        self.player_name = player_name
+
+class Scrabble(Game):
+    def __init__(self, game_type, player_name1, player_name2):
+        super().__init__(game_type)
+        self.player_name1 = player_name1
+        self.player_name2 = player_name2
 
 bingo = Bingo('Bingo', 'Bill')
 print(Game.count)                       # 1
 print(bingo.play())                     # Start the Bingo game!
 print(bingo.player_name)                # Bill
 
-# scrabble = Scrabble('Scrabble', 'Jill', 'Sill')
-# print(Game.count)                       # 2
-# print(scrabble.play())                  # Start the Scrabble game!
-# print(scrabble.player_name1)            # Jill
-# print(scrabble.player_name2)            # Sill
-# print(scrabble.player_name)
-# # AttributeError: 'Scrabble' object has no attribute 'player_name'
+scrabble = Scrabble('Scrabble', 'Jill', 'Sill')
+print(Game.count)                       # 2
+print(scrabble.play())                  # Start the Scrabble game!
+print(scrabble.player_name1)            # Jill
+print(scrabble.player_name2)            # Sill
+print(scrabble.player_name)
+# AttributeError: 'Scrabble' object has no attribute 'player_name'
