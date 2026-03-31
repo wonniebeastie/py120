7ceb60fcf959ecced8c925d34ecbb8b406011fa2
Wonnie <wonnie.tech@gmail.com>
2026-03-31 13:50:41 -0700

    Add initial problem code

---
+++ lesson_4/easy_2/5.py
@@ -0,0 +1,13 @@
+class Greeting:
+    def greet(self, message):
+        print(message)
+
+class Hello(Greeting):
+    def hi(self):
+        self.greet('Hello')
+
+class Goodbye(Greeting):
+    def bye(self):
+        self.greet('Goodbye')
+
+Hello.hi()
