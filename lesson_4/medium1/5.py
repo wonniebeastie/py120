5cc8002195af494c2c088fbe9b663d0c8df76716
Wonnie <wonnie.tech@gmail.com>
2026-04-01 16:06:57 -0700

    Make method name more concise

--- lesson_4/medium1/5.py
+++ lesson_4/medium1/5.py
@@ -3,9 +3,10 @@ class Light:
         self.brightness = brightness
         self.color = color
 
-    def light_status(self):
+    def status(self):
         return (f'I have a brightness level of {self.brightness} '
                 f'and a color of {self.color}')
 
 my_light = Light(50, 'Red')
-print(my_light.light_status())
+print(my_light.status())
+# I have a brightness level of 50 and a color of Red
