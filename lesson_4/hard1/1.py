cf6fbf2ef64b7f0bbf4c2b04d9bdb4c42d18adc6
Wonnie <wonnie.tech@gmail.com>
2026-04-01 16:20:09 -0700

    Add initial problem code

---
+++ lesson_4/hard1/1.py
@@ -0,0 +1,35 @@
+class WheeledVehicle:
+    def __init__(self,
+                 tire_list,
+                 kilometers_per_liter,
+                 liters_of_fuel_capacity):
+        self.tires = tire_list
+        self.fuel_efficiency = kilometers_per_liter
+        self.fuel_capacity = liters_of_fuel_capacity
+
+    def tire_pressure(self, tire_index):
+        return self.tires[tire_index]
+
+    def inflate_tire(self, tire_index, pressure):
+        self.tires[tire_index] = pressure
+
+    def range(self):
+        return self.fuel_capacity * self.fuel_efficiency
+
+class Auto(WheeledVehicle):
+    def __init__(self):
+        # 4 tires with various tire pressures
+        super().__init__([30, 30, 32, 32], 50, 25.0)
+
+class Motorcycle(WheeledVehicle):
+    def __init__(self):
+        # 2 tires with various tire pressures
+        super().__init__([20, 20], 80, 8.0)
+
+class Catamaran:
+    def __init__(self,
+                number_propellers,
+                number_hulls,
+                kilometers_per_liter,
+                liters_of_fuel_capacity):
+        # ... code omitted ...
