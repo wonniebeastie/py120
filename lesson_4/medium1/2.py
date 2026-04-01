e90a56817d4f4e74564f8d45960c955779b01910
Wonnie <wonnie.tech@gmail.com>
2026-04-01 14:52:04 -0700

    Add getter for `quantity` to match output

--- lesson_4/medium1/2.py
+++ lesson_4/medium1/2.py
@@ -3,6 +3,10 @@ class InvoiceEntry:
         self._product_name = product_name
         self._quantity = number_purchased
 
+    @property
+    def quantity(self):
+        return self._quantity
+
 entry = InvoiceEntry('Marbles', 5000)
 print(entry.quantity)         # 5000
 
