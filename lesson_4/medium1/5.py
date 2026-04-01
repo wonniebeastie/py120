bf1027ecfd438acef1054bba780be1e75515ebfa
Wonnie <wonnie.tech@gmail.com>
2026-04-01 16:03:13 -0700

    Add initial problem code
    Goal is to make `light_status` method name

---
+++ lesson_4/medium1/5.py
@@ -0,0 +1,11 @@
+class Light:
+    def __init__(self, brightness, color):
+        self.brightness = brightness
+        self.color = color
+
+    def light_status(self):
+        return (f'I have a brightness level of {self.brightness} '
+                f'and a color of {self.color}')
+
+my_light = Light(50, 'Red')
+print(my_light.light_status())
