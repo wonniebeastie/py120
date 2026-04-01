caaf8f48da8ff7857417ae4d52cc797d8feb9d78
Wonnie <wonnie.tech@gmail.com>
2026-04-01 15:45:27 -0700

    Add initial problem code

---
+++ lesson_4/medium1/4.py
@@ -0,0 +1,16 @@
+class KrispyKreme:
+    def __init__(self, filling, glazing):
+        self.filling = filling
+        self.glazing = glazing
+
+donut1 = KrispyKreme(None, None)
+donut2 = KrispyKreme('Vanilla', None)
+donut3 = KrispyKreme(None, 'sugar')
+donut4 = KrispyKreme(None, 'chocolate sprinkles')
+donut5 = KrispyKreme('Custard', 'icing')
+
+print(donut1)       # Plain
+print(donut2)       # Vanilla
+print(donut3)       # Plain with sugar
+print(donut4)       # Plain with chocolate sprinkles
+print(donut5)       # Custard with icing
