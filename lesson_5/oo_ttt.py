d220404756dcfbb25f82a294c8ba32b86544ef2c
Wonnie <wonnie.tech@gmail.com>
2026-04-08 15:18:57 -0700

    Add a way for the computer to make a move

--- lesson_5/oo_ttt.py
+++ lesson_5/oo_ttt.py
@@ -1,3 +1,5 @@
+import random
+
 class Square:
     INITIAL_MARKER = " "
     HUMAN_MARKER = "X"
@@ -115,6 +117,7 @@ class TTTGame:
                 break
 
             self.computer_moves()
+            self.board.display() # so we can see the computer's move
             if self.is_game_over():
                 break
 
@@ -153,6 +156,8 @@ class TTTGame:
         self.board.mark_square_at(choice, self.human.marker)
 
     def computer_moves(self):
+        choice = random.randint(1, 9)
+        self.board.mark_square_at(choice, self.computer.marker)
         print("computer moves")
 
     def is_game_over(self):
