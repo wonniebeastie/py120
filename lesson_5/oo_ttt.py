2af75c72d461c4c11bb2c32f17d6a65a15ca79b9
Wonnie <wonnie.tech@gmail.com>
2026-04-09 15:58:49 -0700

    Add winning rows as a constant

--- lesson_5/oo_ttt.py
+++ lesson_5/oo_ttt.py
@@ -96,6 +96,17 @@ class Computer(Player):
         super().__init__(Square.COMPUTER_MARKER)
 
 class TTTGame:
+    POSSIBLE_WINNING_ROWS = (
+        (1, 2, 3),  # top row of board
+        (4, 5, 6),  # center row of board
+        (7, 8, 9),  # bottom row of board
+        (1, 4, 7),  # left column of board
+        (2, 5, 8),  # middle column of board
+        (3, 6, 9),  # right column of board
+        (1, 5, 9),  # diagonal: top-left to bottom-right
+        (3, 5, 7),  # diagonal: top-right to bottom-left
+    )
+
     def __init__(self):
         self.board = Board()
         self.human = Human()
