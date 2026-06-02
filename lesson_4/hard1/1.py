13f7241516aa6eda6b56a4f0f8eb40e4ffa7b5df
Wonnie <wonnie.tech@gmail.com>
2026-06-02 13:15:24 -0700

    Problem #1 of hard problems

--- lesson_4/hard1/1.py
+++ lesson_4/hard1/1.py
@@ -1,4 +1,7 @@
 """
+Goal: Modify the class definitions & move code into a mix-in so that code can
+be shared between the wheeled vehicles & `Catamaran`.
+
 Rules:
     - `Auto` represents automobiles
     - `Motorcycles` represent motorcycles
@@ -19,8 +22,14 @@ Mixin Algo:
     - set fuel capacity
 """
 class FueledVehicleMixin:
-    def set_fuel_efficiency(self):
-        self.fuel_efficiency = self.kilometers_per_liter
+    def range(self):
+        return self.fuel_capacity * self.fuel_efficiency
+
+    def set_fuel_efficiency(self, kilometers_per_liter):
+        self.fuel_efficiency = kilometers_per_liter
+
+    def set_fuel_capacity(self, liters):
+        self.fuel_capacity = liters
 
 class WheeledVehicle(FueledVehicleMixin):
     def __init__(self,
@@ -28,17 +37,14 @@ class WheeledVehicle(FueledVehicleMixin):
                  kilometers_per_liter,
                  liters_of_fuel_capacity):
         self.tires = tire_list
-        self.fuel_efficiency = kilometers_per_liter
-        self.fuel_capacity = liters_of_fuel_capacity
+        self.set_fuel_efficiency(kilometers_per_liter)
+        self.set_fuel_capacity(liters_of_fuel_capacity)
 
     def tire_pressure(self, tire_index):
         return self.tires[tire_index]
 
     def inflate_tire(self, tire_index, pressure):
         self.tires[tire_index] = pressure
-
-    def range(self):
-        return self.fuel_capacity * self.fuel_efficiency
 
 class Auto(WheeledVehicle):
     def __init__(self):
@@ -56,7 +62,23 @@ class Catamaran(FueledVehicleMixin):
                 number_hulls,
                 kilometers_per_liter,
                 liters_of_fuel_capacity):
-        self.number_propellers = number_propellers
-        self.number_hulls = number_hulls
-        self.kilometers_per_liter = kilometers_per_liter
-        self.liters_of_fuel_capacity = liters_of_fuel_capacity
+        self.propellers = number_propellers
+        self.hulls = number_hulls
+        self.set_fuel_capacity(liters_of_fuel_capacity)
+        self.set_fuel_efficiency(kilometers_per_liter)
+
+auto = Auto()
+motorcycle = Motorcycle()
+catamaran = Catamaran(2, 2, 1.5, 600)
+
+print(auto.fuel_efficiency)             # 50
+print(auto.fuel_capacity)               # 25.0
+print(auto.range())                     # 1250.0
+
+print(motorcycle.fuel_efficiency)       # 80
+print(motorcycle.fuel_capacity)         # 8.0
+print(motorcycle.range())               # 640.0
+
+print(catamaran.fuel_efficiency)        # 1.5
+print(catamaran.fuel_capacity)          # 600
+print(catamaran.range())                # 900.0
